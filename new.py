d46b498caa49d44983b17d7fb128e54e0244d21b
bendsouza <bendsouza2@gmail.com>
2022-11-18 15:55:07 +0000

    normalised the data, added xgboost object and tested model for accuracy

--- new.py
+++ new.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import numpy as np
-import os, sys
 from sklearn.preprocessing import MinMaxScaler
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
@@ -14,4 +12,18 @@ x = df.loc[:, df.columns != "status"].to_numpy()[:, 1:]  # features
 y = df.loc[:, 'status'].to_numpy()  # labels
 
 print(y[y == 1].shape[0], y[y == 0].shape[0])  # number of patients with (1) and without (0) Parkinson's
+
+# Normalising the data
+scaler = MinMaxScaler((-1, 1))
+features = scaler.fit_transform(x)
+
+x_train, x_test, y_train, y_test = train_test_split(features, y, test_size=0.2, random_state=7)  #splitting
+
+#  Building the model
+xgb = XGBClassifier()
+xgb.fit(x_train, y_train)
+
+#  Accuracy
+pred_y = xgb.predict(x_test)
+print(accuracy_score(y_test, pred_y) * 100)
 
